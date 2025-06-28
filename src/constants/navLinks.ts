464027caf461b9c5070ea66e86ec1dd40d5524a8
andrew-solarquotes <andrew@solarquotes.com.au>
2025-06-28 16:02:02 +1000

    fix: rewording to 'Portfolio'

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
   { href: '/about-us', label: 'About Us' },
-  { href: '/our-portfolio', label: 'Our Portfolio' },
+  { href: '/portfolio', label: 'Portfolio' },
   // { href: '/news', label: 'News' },
   { href: '/contact-us', label: 'Contact Us' },
 ];
